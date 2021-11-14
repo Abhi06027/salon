5215291887cb58419912cb5205f39be573d7e07d
Abhi06027 <abhinavk67@gmail.com>
2021-11-14 22:10:58 +0530

    salon web

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+    colors: {
+     primary: '#150303',
+     secondary: {
+       100: '#142F43',
+       200: '#FF5F7E',
+     }
+
+    },
+   fontFamily: {
+     body: ['Nunito', ]
+   }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
